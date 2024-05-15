07c9b402c4c9bfc3c1d3447047d07b8ad9198fe3
VuacheslavMironov <vuacheslav.mir@gmail.com>
2024-05-15 17:09:40 +0700

    Сделал набросок страниц логин, регистрация, ресет пароля

---
+++ sources/resources/views/layouts/notification.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+    </head>
+    <body>
+        <! -- HEADER -->
+        <div style="background-color: #610808;">
+            <img src="{{ asset('images/logo.png') }}" alt="{{ $link }}">
+        </div>
+        @yield('content')
+        <! -- FOOTER -->
+        <div style="background-color: #222; color: #fff">
+            <div style="text-align: center;">
+                <p>
+                    <b>Хорошего дня!</b>
+                </p>
+                <p>С уважением, команда SportExperts.</p>
+            </div>
+        </div>
+    </body>
+</html>
