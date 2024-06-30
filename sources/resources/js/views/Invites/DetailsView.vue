d3a4ce00789408984ad67e1a4aea4d1a7a523160
VuacheslavMironov <vuacheslav.mir@gmail.com>
2024-07-01 00:16:43 +0700

    Початок для вывода спортсменов и их событий

---
+++ sources/resources/js/views/Invites/DetailsView.vue
@@ -0,0 +1,67 @@
+<script>
+import {
+    BASE_URL,
+    IDENTIFIER,
+    MESSAGES,
+    ENDPOINTS,
+    RESPONSE
+} from "../../constant";
+import Card from 'primevue/card';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import InputMask from 'primevue/inputmask';
+import FloatLabel from 'primevue/floatlabel';
+import Message from 'primevue/message';
+
+export default {
+    data()
+    {
+        return {
+            messageSuccess: null,
+            messageError: null,
+            invites: [],
+            participants: [],
+        };
+    },
+    methods: {
+
+    },
+    components: {
+        Card: Card,
+        InputText: InputText,
+        Button: Button,
+        InputMask: InputMask,
+        FloatLabel: FloatLabel,
+        Message: Message,
+    },
+    beforeMount() {
+
+    }
+}
+</script>
+
+<template>
+    <section class="d-flex d-center">
+        <div class="text-center mt-3 mb-3">
+            <h2>Спортсмены которых вы пригласили.</h2>
+        </div>
+    </section>
+    <section class="mt-1 mb-2" v-if="this.messageSuccess">
+        <Message severity="success">{{ this.messageSuccess }}</Message>
+    </section>
+    <section class="mt-1 mb-2" v-if="this.messageError">
+        <Message severity="error">{{ this.messageError }}</Message>
+    </section>
+    <section class="d-flex d-between container">
+        <div class="w-30">
+            <section class="w-100">
+                <Card>
+                    <template #content></template>
+                </Card>
+            </section>
+        </div>
+    </section>
+    <!--    <section v-if="this.events && this.events[this.response.data].length > 9" class="mt-5 mb-5">-->
+    <!--    <Paginator :rows="9" :totalRecords="120"></Paginator>-->
+    <!--    </section>-->
+</template>
--- sources/resources/js/views/Invites/IndexView.vue
+++ sources/resources/js/views/Invites/IndexView.vue
@@ -6,23 +6,34 @@ import {
     ENDPOINTS,
     RESPONSE
 } from "../../constant";
-import { registrationRequest } from '../../api/UserRequest';
-import { loggingRequest } from '../../api/LoggingRequest';
 import Card from 'primevue/card';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import InputMask from 'primevue/inputmask';
 import FloatLabel from 'primevue/floatlabel';
 import Message from 'primevue/message';
+import InlineMessage from 'primevue/inlinemessage';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import Row from 'primevue/row';
+import {listInvitedRequest} from "../../api/InvitedRequest";
+import {loggingRequest} from "../../api/LoggingRequest";
 
 export default {
     data() {
         return {
             baseUrl: BASE_URL,
-            messageError: null,
+            noData: MESSAGES.NO_DATA,
             response: RESPONSE,
             route: ENDPOINTS,
             currentDate: new Date(),
+            user: null,
+            messageSuccess: null,
+            messageError: null,
+            invites: [],
+            participants: [],
+            expandedRows: {}
         };
     },
     components: {
@@ -32,12 +43,134 @@ export default {
         InputMask: InputMask,
         FloatLabel: FloatLabel,
         Message: Message,
+        InlineMessage: InlineMessage,
+        DataTable: DataTable,
+        Column: Column,
+        ColumnGroup: ColumnGroup,
+        Row: Row
     },
-    methods: {},
-    beforeMount() {}
+    methods: {
+        formatDate: (inputDate) => {
+            const [year, month, day] = inputDate.split('-');
+            return `${day.padStart(2, '0')}.${month.padStart(2, '0')}.${year}`;
+        },
+        onRowExpand(event) {
+            this.$toast.add({ severity: 'info', summary: 'Product Expanded', detail: event.data.name, life: 3000 });
+        },
+        onRowCollapse(event) {
+            this.$toast.add({ severity: 'success', summary: 'Product Collapsed', detail: event.data.name, life: 3000 });
+        },
+        expandAll() {
+            this.expandedRows = this.invites.reduce((acc, p) => (acc[p.id] = true) && acc, {});
+        },
+        collapseAll() {
+            this.expandedRows = null;
+        },
+        getUserInvites: async function()
+        {
+            let attributes = {
+                who_user_id: window.$cookies.get(IDENTIFIER)
+            };
+            await listInvitedRequest(attributes)
+                .then((response) => { this.invites = response.data.result.original; })
+                .catch((error) => {
+                    loggingRequest({
+                        current_date: `${this.currentDate.getDate().toString().padStart(2, '0')}-${(this.currentDate.getMonth() + 1).toString().padStart(2, '0')}-${this.currentDate.getFullYear()}`,
+                        current_time: `${this.currentDate.getHours().toString().padStart(2, '0')}:${this.currentDate.getMinutes().toString().padStart(2, '0')}:${this.currentDate.getSeconds().toString().padStart(2, '0')}`,
+                        method: 'listInvitedRequest',
+                        status: error.code,
+                        request_data: attributes.toString(),
+                        message: error.message
+                    });
+                    this.messageError = MESSAGES.NO_DATA;
+                });
+        }
+    },
+    async beforeMount() {
+        await this.getUserInvites();
+    }
 }
 </script>
 
 <template>
-
+    <section class="d-flex d-center">
+        <div class="text-center mt-3 mb-3">
+            <h2>Спортсмены которых вы пригласили.</h2>
+        </div>
+    </section>
+    <section class="mt-1 mb-2" v-if="this.messageSuccess">
+        <Message severity="success">{{ this.messageSuccess }}</Message>
+    </section>
+    <section class="mt-1 mb-2" v-if="this.messageError">
+        <Message severity="error">{{ this.messageError }}</Message>
+    </section>
+    <section class="d-flex d-between container">
+            <section class="w-100">
+                <Card v-if="this.invites && this.invites.length">
+                    <template #content>
+                        <DataTable v-model:expandedRows="expandedRows"
+                                   :value="this.invites"
+                                   dataKey="id"
+                                   @rowExpand="onRowExpand"
+                                   @rowCollapse="onRowCollapse"
+                                   tableStyle="min-width: 60rem">
+                            <template #header>
+                                <div class="flex flex-wrap justify-content-end gap-2">
+                                    <Button text icon="pi pi-cloud-download" label="Скачать документом" @click="collapseAll" />
+                                </div>
+                            </template>
+                            <Column expander style="width: 5rem" />
+                            <Column field="user.last_name" header="Фамилия" sortable></Column>
+                            <Column field="user.first_name" header="Имя" sortable></Column>
+                            <Column header="Дата рождения" sortable>
+                                <template #body="slotProps">
+                                    {{ this.formatDate(slotProps.data.user.birth_date) }}
+                                </template>
+                            </Column>
+                            <Column field="user.email" header="Почта"></Column>
+                            <Column field="user.phone" header="Номер телефона"></Column>
+                            <template #expansion="slotProps">
+                                <h5>Мероприятия на которые записан спортсмен (5 шт)</h5>
+                                <DataTable :value="this.participants">
+                                    <Column header="Баннер" sortable>
+                                        <template #body="slotProps">
+                                            <div :style="
+                                                    'background-size: cover;' +
+                                                    'background-position: top;' +
+                                                    'background-image: url(' + this.baseUrl + 'storage/uploads/' + slotProps.data.event.image.name + ');' +
+                                                    'height: 8em;' +
+                                                    'width: 50%;' +
+                                                    'background-repeat: no-repeat;'
+                                            "></div>
+                                        </template>
+                                    </Column>
+                                    <Column field="event.name" header="Название" sortable></Column>
+                                    <Column header="Статус" sortable>
+                                        <template #body="slotProps">
+                                            <section v-if="slotProps.data.event.status === 'Active'">
+                                                <Tag severity="success" value="Активен"></Tag>
+                                            </section>
+                                            <section v-else-if="slotProps.data.event.status === 'No active'">
+                                                <Tag severity="info" value="Завершен"></Tag>
+                                            </section>
+                                            <section v-else>
+                                                <Tag severity="warning" value="В архиве"></Tag>
+                                            </section>
+                                        </template>
+                                    </Column>
+                                    <Column field="event.start_date" header="Дата старта" sortable></Column>
+                                    <Column field="event.start_time" header="Время старта" sortable></Column>
+                                </DataTable>
+                            </template>
+                        </DataTable>
+                    </template>
+                </Card>
+                <section v-else class="d-flex d-center">
+                    <InlineMessage severity="info" class="w-50 mt-5">{{ this.noData }}</InlineMessage>
+                </section>
+            </section>
+    </section>
+    <!--    <section v-if="this.events && this.events[this.response.data].length > 9" class="mt-5 mb-5">-->
+    <!--    <Paginator :rows="9" :totalRecords="120"></Paginator>-->
+    <!--    </section>-->
 </template>
